e3e824cb282b243d63c89d8d3e9598c71e21e25a
Atef Ben Ali <atef.bettaib@gmail.com>
2021-11-28 08:19:07 +0100

    init commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,65 @@
+import type { MetaFunction, LoaderFunction } from "remix";
+import { useLoaderData, json, Link } from "remix";
+
+type IndexData = {
+  resources: Array<{ name: string; url: string }>;
+  demos: Array<{ name: string; to: string }>;
+};
+
+// Loaders provide data to components and are only ever called on the server, so
+// you can connect to a database or run any server side code you want right next
+// to the component that renders it.
+// https://remix.run/api/conventions#loader
+export let loader: LoaderFunction = () => {
+  let data: IndexData = {
+    demos: [
+      {
+        to: "demos/actions",
+        name: "Actions",
+      },
+      {
+        to: "demos/about",
+        name: "Nested Routes, CSS loading/unloading",
+      },
+      {
+        to: "demos/params",
+        name: "URL Params and Error Boundaries",
+      },
+    ],
+  };
+
+  return json(data);
+};
+
+// https://remix.run/api/conventions#meta
+export let meta: MetaFunction = () => {
+  return {
+    title: "Remix test app",
+    description: "Welcome !",
+  };
+};
+
+// https://remix.run/guides/routing#index-routes
+export default function Index() {
+  let data = useLoaderData<IndexData>();
+
+  return (
+    <div className="remix__page">
+      <main>
+        <h2>Welcome to Remix!</h2>
+      </main>
+      <aside>
+        <h2>Demos In This App</h2>
+        <ul>
+          {data.demos.map((demo) => (
+            <li key={demo.to} className="remix__page__resource">
+              <Link to={demo.to} prefetch="intent">
+                {demo.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </aside>
+    </div>
+  );
+}
